876b787f730253f49ccb13f6e64c1a351c471e07
sondzu <sonvhe151371@fpt.edu.vn>
2023-07-22 23:23:23 +0700

    Update VM_RealEstate.cs

--- RAS/HakunaMatata/Models/ViewModels/VM_RealEstate.cs
+++ RAS/HakunaMatata/Models/ViewModels/VM_RealEstate.cs
@@ -3,19 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HakunaMatata.Models.ViewModels
 {
-    public class VM_RealEstate
+    public class VM_News
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        
-        [DisplayFormat(DataFormatString ="dd/MM/yyyy")]
-        public DateTime PostTime { get; set; }
-
-        [DisplayFormat(DataFormatString = "dd/MM/yyyy")]
-        public DateTime? ExprireTime { get; set; }
-        public int RealEstateTypeId { get; set; }
-        public decimal Price { get; set; }
-        public string AgentName { get; set; }
-        public bool IsActive { get; set; }
+        public string NewsBody { get; set; }
+        public string UrlImage { get; set; }
     }
 }
