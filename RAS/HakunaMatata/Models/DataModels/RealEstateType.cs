8595b931c814dc2b68dcd8fefd7170160d3b6e42
Tedadeza <datnthe150015@fpt.edu.vn>
2023-07-27 23:40:50 +0700

    validation

--- RAS/HakunaMatata/Models/DataModels/RealEstateType.cs
+++ RAS/HakunaMatata/Models/DataModels/RealEstateType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HakunaMatata.Models.DataModels
 {
@@ -11,6 +12,9 @@ namespace HakunaMatata.Models.DataModels
         }
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "RealEstateTypeName is required.")]
+        [StringLength(50, ErrorMessage = "RealEstateTypeName must not exceed 50 characters.")]
         public string RealEstateTypeName { get; set; }
 
         public virtual ICollection<RealEstate> RealEstate { get; set; }
