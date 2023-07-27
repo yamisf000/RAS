01126c458285419647cb594fd05c0cc98c196316
Tedadeza <datnthe150015@fpt.edu.vn>
2023-07-27 23:48:49 +0700

    valid level

--- RAS/HakunaMatata/Models/DataModels/Level.cs
+++ RAS/HakunaMatata/Models/DataModels/Level.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HakunaMatata.Models.DataModels
 {
@@ -11,6 +12,10 @@ namespace HakunaMatata.Models.DataModels
         }
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "LevelName is required.")]
+        [StringLength(50, ErrorMessage = "LevelName must not exceed 50 characters.")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "LevelName must contain letters and spaces only.")]
         public string LevelName { get; set; }
 
         public virtual ICollection<Agent> Agent { get; set; }
