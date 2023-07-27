611a164cd38497bb902c75be5607ecda8102a427
Tedadeza <datnthe150015@fpt.edu.vn>
2023-07-27 23:52:06 +0700

    update thông báo

--- RAS/HakunaMatata/Models/ViewModels/VM_Login.cs
+++ RAS/HakunaMatata/Models/ViewModels/VM_Login.cs
@@ -4,11 +4,11 @@ namespace HakunaMatata.Models.ViewModels
 {
     public class VM_Login
     {
-        [Required]
+        [Required(ErrorMessage = "LoginName is required.")]
         public string LoginName { get; set; }
 
-        [Required]
-        [MinLength(6)]
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
 
         public bool IsRememberMe { get; set; }
@@ -16,4 +16,3 @@ namespace HakunaMatata.Models.ViewModels
         public string ReturnUrl { get; set; }
     }
 }
-
