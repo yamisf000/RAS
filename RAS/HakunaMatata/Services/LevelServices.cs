044374c399e4a60567d24fc0b5486879d715852a
linhle1512 <98157359+linhle1512@users.noreply.github.com>
2023-07-29 02:01:53 +0700

    Revert "public interface GenerateOrderReport"
    This reverts commit 051642c8f59aeb2127091a553e28bdc94384c7ff.

--- RAS/HakunaMatata/Services/LevelServices.cs
+++ RAS/HakunaMatata/Services/LevelServices.cs
@@ -1,6 +1,5 @@
 ﻿using HakunaMatata.Data;
 using HakunaMatata.Models.DataModels;
-using HakunaMatata.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +9,6 @@ namespace HakunaMatata.Services
 {
     public interface ILevelServices
     {
-        Task<List<ChartReportSummaryModel>> GenerateOrderReport(int year, int month);
         IEnumerable<Level> GetListLevels();
         Level GetDetails(int id);
         void Create(Level level);
