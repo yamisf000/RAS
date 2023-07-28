32eaeae17f02dc35f84865a27892db6eecd41b6f
linhle1512 <98157359+linhle1512@users.noreply.github.com>
2023-07-29 02:25:38 +0700

    add NewsController

---
+++ RAS/HakunaMatata/Areas/AdminArea/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using HakunaMatata.Helpers;
+using HakunaMatata.Models.DataModels;
+using HakunaMatata.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HakunaMatata.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    [Authorize(Roles = "Admin")]
+    public class NewsController : Controller
+    {
+        private readonly ILevelServices _services;
+        public NewsController(ILevelServices services)
+        {
+            _services = services;
+        }
+      
+    }
+}
